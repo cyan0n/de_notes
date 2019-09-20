4409a342c55e0e285df5b580b8e8a4099aa9abc5
Cyan0n <cyan0n.dev@gmail.com>
2019-09-20 10:14:07 +0200

    Basic App styling

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,15 +1,32 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+	<div id="app">
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
-  export default {
-    name: 'de_notes'
-  }
+export default {
+	name: 'de_notes'
+}
 </script>
 
-<style>
-  /* CSS */
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+}
+
+body {
+	height: 100vh;
+	width: 100vw;
+	padding: 20px;
+	font-family: 'Source Sans Pro', sans-serif;
+	background:
+		radial-gradient(ellipse at top left,
+			rgba(255, 255, 255, 1) 40%,
+			rgba(219, 219, 219, .9) 100%);
+}
 </style>
